9dc68aa55f959566062bfa5a3a42b8e992e2418d
wojLi <wojciech.ligeza@icloud.com>
2022-11-03 15:49:23 +0100

    build: setup project

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+/** @type {import("prettier").RequiredOptions} */
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+}
