df936030d9ec0bb194e7501040fb31c366744239
wojLi <wojciech.ligeza@icloud.com>
2023-01-22 16:27:46 +0100

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { securityHeaders } from './common/middleware/security-headers.middleware';
@@ -13,6 +14,14 @@ async function bootstrap() {
   app.enableCors({ origin: process.env.CORS_ORIGIN });
   app.use(securityHeaders);
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Sentiment Analysis Web API')
+    .setDescription('API to predict the analysis of provided text')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const prismaService = app.select(PrismaClientModule).get(PrismaClientService, { strict: true });
   await prismaService.enableShutdownHooks(app);
